064a66bb8b88fd50af72bf745db89a7964d5bb03
yahaa <1477765176@qq.com>
2021-03-21 21:41:41 +0800

    add leetcode 310 todo

--- algo-rust/src/graph/graph.rs
+++ algo-rust/src/graph/graph.rs
@@ -1,4 +1,7 @@
 #![allow(dead_code)]
+use std::cmp::min;
+use std::collections::HashMap;
+use std::collections::VecDeque;
 
 struct Solution {}
 
@@ -73,10 +76,80 @@ impl Solution {
 
         return vec![];
     }
+
+    // leetcode 310
+    pub fn find_min_height_trees(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut graph = vec![vec![]; n as usize];
+
+        for e in edges {
+            graph[e[0] as usize].push(e[1] as usize);
+            graph[e[1] as usize].push(e[0] as usize);
+        }
+
+        let mut hmap: HashMap<i32, Vec<i32>> = HashMap::new();
+        let mut min_high = n;
+
+        for root in 0..n as usize {
+            let high = Solution::tree_hight(root, &graph, n);
+
+            let list = hmap.entry(high).or_insert(vec![]);
+            list.push(root as i32);
+
+            min_high = min(min_high, high);
+        }
+
+        match hmap.remove(&min_high) {
+            Some(res) => res,
+            None => vec![],
+        }
+    }
+
+    // leetcode 310 help, find the high of the tree
+    fn tree_hight(root: usize, graph: &Vec<Vec<usize>>, n: i32) -> i32 {
+        let mut visit = vec![false; n as usize];
+        let (mut queue, mut high) = (VecDeque::new(), 0);
+
+        queue.push_back(root);
+
+        while !queue.is_empty() {
+            let mut size = queue.len();
+
+            while size > 0 {
+                let s = queue.pop_front().unwrap();
+                visit[s] = true;
+
+                for e in graph[s].iter() {
+                    if !visit[*e] {
+                        queue.push_back(*e);
+                    }
+                }
+
+                size -= 1;
+            }
+
+            high += 1;
+        }
+
+        high
+    }
 }
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn find_min_height_trees() {
+        let edges = vec![vec![1, 0], vec![1, 2], vec![1, 3]];
+
+        assert_eq!(vec![1], Solution::find_min_height_trees(4, edges));
+
+        let edges = vec![vec![3, 0], vec![3, 1], vec![3, 2], vec![3, 4], vec![5, 4]];
+
+        assert_eq!(vec![3, 4], Solution::find_min_height_trees(6, edges));
+
+        let edges = vec![vec![0, 1], vec![0, 2], vec![0, 3], vec![3, 4], vec![4, 5]];
+        assert_eq!(vec![3], Solution::find_min_height_trees(6, edges));
+    }
 
     #[test]
     fn can_finish() {
