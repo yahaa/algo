f0f6dd44ef723444aeead4105b6e86897af1fb90
yahaa <1477765176@qq.com>
2021-09-12 23:58:47 +0800

    leetcode 1173

--- algo-rust/src/search/search.rs
+++ algo-rust/src/search/search.rs
@@ -504,6 +504,17 @@ impl Solution {
     pub fn interpret(command: String) -> String {
         command.replace("(al)", "al").replace("()", "o")
     }
+
+    // leetcode 1173
+    pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+        let key = match rule_key.as_str() {
+            "type" => 0,
+            "color" => 1,
+            _ => 2,
+        };
+
+        items.iter().filter(|x| x[key] == rule_value).count() as i32
+    }
 }
 
 #[cfg(test)]
