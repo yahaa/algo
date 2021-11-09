8226fda926ab7b9207301453b5ff8ca5f7b5bc2f
yahaa <1477765176@qq.com>
2021-11-10 00:10:35 +0800

    feat: leetcode 128 longest consecutive sequence

--- algo-rust/src/hash/hash.rs
+++ algo-rust/src/hash/hash.rs
@@ -23,6 +23,45 @@ impl Solution {
 
         result
     }
+
+    // Longest Consecutive Sequence
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+
+        let (mut map, mut ans) = (HashMap::new(), 1);
+
+        for n in nums {
+            if map.contains_key(&n) {
+                continue;
+            }
+
+            map.insert(n, 1);
+
+            let (mut left, mut right, mut length) = (n, n, 1);
+            if let Some(v) = map.get(&(n - 1)) {
+                left = n - v;
+                length += match map.get(&left) {
+                    Some(vl) => *vl,
+                    None => 0,
+                };
+            };
+
+            if let Some(v) = map.get(&(n + 1)) {
+                right = n + v;
+                length += match map.get(&right) {
+                    Some(vr) => *vr,
+                    None => 0,
+                };
+            }
+
+            map.insert(left, length);
+            map.insert(right, length);
+            ans = std::cmp::max(ans, length);
+        }
+        ans
+    }
 }
 
 #[cfg(test)]
@@ -35,5 +74,12 @@ mod test {
         let want = vec![vec![0, 1, 2], vec![5], vec![3, 4, 6]];
 
         assert_eq!(want, Solution::group_the_people(group_sizes));
+    }
+    #[test]
+    fn longest_consecutive() {
+        let nums = vec![100, 4, 200, 1, 3, 2];
+        let want = 4;
+
+        assert_eq!(want, Solution::longest_consecutive(nums));
     }
 }
