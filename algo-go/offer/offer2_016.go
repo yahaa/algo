ac7a9f35215e86912d7a27eef90dc5f0738bc803
yahaa <1477765176@qq.com>
2023-07-13 23:29:26 +0800

    leetcode 3 || offer2_013

---
+++ algo-go/offer/offer2_016.go
@@ -0,0 +1,21 @@
+package offer
+
+func lengthOfLongestSubstring_2(s string) int {
+	i, j, ans, count := 0, 0, 0, make(map[byte]int)
+
+	for i <= j && j < len(s) {
+		if _, ok := count[s[j]]; !ok {
+			count[s[j]]++
+			j++
+		} else {
+			delete(count, s[i])
+			i++
+		}
+
+		if j-i > ans {
+			ans = j - i
+		}
+	}
+
+	return ans
+}
