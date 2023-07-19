dfe0372222b151f4ac31bfe8b54abdf7f810797f
yahaa <1477765176@qq.com>
2023-07-20 00:26:39 +0800

    leetcode 233 && offer 43

--- algo-go/offer/offer43.go
+++ algo-go/offer/offer43.go
@@ -2,44 +2,47 @@ package offer
 
 // leetcode 43
 func countDigitOne(n int) int {
-	// "1"出现的次数 = sum ("1"在各个计数位上出现的次数)
-	// 从个位开始向最高位统计
-	// 3101592
-	// 将数字拆分为[a...][cur][b...]
-	// cur 为当前位
-	var base, res int64 = 1, 0
-	n64 := int64(n)
-	for base <= n64 {
-		// 计算 a..., cur, b...
-		a := n64 / base / 10
-		cur := (n64 / base) % 10
-		b := n64 % base
-		// 将当前位设为1，考察其他部分的变化范围
+	// n 划分为三个部位 3101592
+	// cur 为 base 位的值
+	// a 为 base 位左边的值
+	// b 为 base 位右边剩下的值
+	// [a][cur][b]
+
+	// 如果 cur > 1
+	// a 的范围为 [0-a]，总共 a+1 个
+	// b 的范围为 [0-base]，总共 base 个
+	// 总的个数为 (a+1)*base
+
+	// 如果 cur == 1
+	// a 的范围为 [0-a],总共 a+1 个
+	// 当 a 的范围取最大值 "a" 时， b 的范围为 [0-b] 总共 b+1 个
+	// 当 a 的范围为 [0-a) 时，b 的范围为 [0-base_1] 总共 base 个
+	// 总个数为 b+1+a*base
+
+	// 如果 cur < 1
+	// a 的范围为 [0-a), 总共 a 个，为什么 a 的范围不能是 [0-a]?
+	//    因为我们求的是 base 位为 1 ,
+	//    当我们把 base 位设置为 1 后 a 的范围是 [0-a],那么当前数字将会 > n
+	// 当 a 的范围为 [0-a) 时，b 的范围为 [0-base_1] 总共 base 个
+	// 总共 (a)*(base) 个
+
+	base := 1
+	res := 0
+	for base <= n {
+		a := n / base / 10
+		cur := (n / base) % 10
+		b := n % base
+
 		if cur > 1 {
-			// 一、cur > 1，
-			//          [3101 ] 5 [92]
-			// 变化范围：[0-3101] 1 [0-99]
-			// 总个数：   (a+1)  *  base
 			res += (a + 1) * base
 		} else if cur == 1 {
-			// 二、cur == 1，
-			//             [310] 1 [592]
-			// 1、变化范围 [0-309] 1 [0-999]
-			//              a    *  base
-			// 2、变化范围 [310]   1 [0-592]
-			//               1   *   (b+1)
-			// 总个数：a *base + (b + 1)
 			res += a*base + b + 1
-
 		} else {
-			// 三、cur < 1，
-			//           [31] 0 [1592]
-			// 变化范围 [0-30] 1 [0-9999]
-			// 总个数    a     *   base
 			res += a * base
 		}
-		// 统计更高一位
+
 		base *= 10
 	}
-	return int(res)
+
+	return res
 }
