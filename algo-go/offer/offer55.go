09225121785d926738a932a3a5e57d06fc00f41a
yahaa <1477765176@qq.com>
2023-08-01 23:36:16 +0800

    offer2_55

--- algo-go/offer/offer55.go
+++ algo-go/offer/offer55.go
@@ -7,3 +7,27 @@ func maxDepth(root *TreeNode) int {
 
 	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
 }
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+
+	return -a
+}
+
+func depth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return max(depth(root.Left), depth(root.Right)) + 1
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return abs(depth(root.Left)-depth(root.Right)) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+}
