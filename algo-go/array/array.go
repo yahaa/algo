f90771347488451db8de0c80b729d61e4046ea63
yahaa <1477765176@qq.com>
2023-08-13 23:01:55 +0800

    leetcode 88 合并 2 个有序数组，结果保存在 nums1 中

--- algo-go/array/array.go
+++ algo-go/array/array.go
@@ -160,3 +160,27 @@ func ProjectionArea(grid [][]int) int {
 	}
 	return res
 }
+
+// merge leetcode 88
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+	for k >= 0 {
+		if i >= 0 && j >= 0 {
+			if nums1[i] < nums2[j] {
+				nums1[k] = nums2[j]
+				j--
+			} else {
+				nums1[k] = nums1[i]
+				i--
+			}
+		} else if i >= 0 {
+			nums1[k] = nums1[i]
+			i--
+		} else if j >= 0 {
+			nums1[k] = nums2[j]
+			j--
+		}
+
+		k--
+	}
+}
