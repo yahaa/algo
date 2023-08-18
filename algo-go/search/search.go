fec1a312690fb090a517a812a1ecddb104cc6a60
yahaa <1477765176@qq.com>
2023-08-18 23:21:02 +0800

    leetcode 704 二分搜索递归实现

--- algo-go/search/search.go
+++ algo-go/search/search.go
@@ -18,3 +18,31 @@ func search(nums []int, target int) int {
 
 	return -1
 }
+
+func search2(nums []int, target int) int {
+	var dfs func(l, r int)
+	ans := -1
+	dfs = func(l, r int) {
+		if l >= r {
+			return
+		}
+
+		m := l + (r-l)/2
+		if nums[m] == target {
+			ans = m
+			return
+		}
+
+		if nums[m] < target {
+			l = m + 1
+		} else {
+			r = m
+		}
+
+		dfs(l, r)
+	}
+
+	dfs(0, len(nums))
+
+	return ans
+}
