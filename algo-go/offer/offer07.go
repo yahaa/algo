9fc59ab15cbda97329b09ce6260dcc85725d5021
yahaa <1477765176@qq.com>
2023-08-20 23:29:40 +0800

    offer 07 先序遍历中序遍历重建二叉树

--- algo-go/offer/offer07.go
+++ algo-go/offer/offer07.go
@@ -28,5 +28,26 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	root.Right = buildTree(preorder[1+rootIndex:], inorder[rootIndex+1:])
 
 	return root
+}
 
+// buildTree2  offer 07 先序遍历中序遍历重建二叉树
+func buildTree2(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{Val: preorder[0]}
+
+	i := 0
+
+	for i = 0; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+
+	root.Left = buildTree2(preorder[1:i+1], inorder[:i])
+	root.Right = buildTree2(preorder[i+1:], inorder[i+1:])
+
+	return root
 }
