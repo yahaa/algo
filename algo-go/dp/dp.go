5fbc3146d49c644f2798a831b0707f96786b654c
yahaa <1477765176@qq.com>
2023-08-25 23:01:12 +0800

    leetcode 1143 最长公共子序列

--- algo-go/dp/dp.go
+++ algo-go/dp/dp.go
@@ -130,6 +130,31 @@ func lengthOfLIS(nums []int) int {
 	return ans
 }
 
+// longestCommonSubsequence leetcode 1143 最长公共子序列
+func longestCommonSubsequence(text1 string, text2 string) int {
+	// 定义 dp[i][j] 为一 i,j 结尾的字符串最长的公共子序列
+	// 当 text1[i] == text2[j] dp[i][j]=dp[i-1][j-1]+1
+	// 当 text1[i] != text2[j] dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+	n, m := len(text1), len(text2)
+	dp := make([][]int, n+1)
+
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[n][m]
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
