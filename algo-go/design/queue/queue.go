04be57141394a3ff935ef0d41362f38e785f0870
yahaa <1477765176@qq.com>
2023-08-26 23:37:41 +0800

    leetcode 1485 队列最大值 && offer 59

--- algo-go/design/queue/queue.go
+++ algo-go/design/queue/queue.go
@@ -1,5 +1,9 @@
 package queue
 
+import (
+	"container/list"
+)
+
 type MyCircularQueue struct {
 	size      int
 	count     int
@@ -75,4 +79,58 @@ func (cir *MyCircularQueue) IsFull() bool {
  * param_4 := obj.Rear();
  * param_5 := obj.IsEmpty();
  * param_6 := obj.IsFull();
+ */
+
+type MaxQueue struct {
+	dataQ *list.List
+	maxQ  *list.List
+}
+
+func Constructor2() MaxQueue {
+	return MaxQueue{
+		dataQ: list.New(),
+		maxQ:  list.New(),
+	}
+
+}
+
+func (this *MaxQueue) Max_value() int {
+	if this.maxQ.Len() <= 0 {
+		return -1
+	}
+
+	return this.maxQ.Front().Value.(int)
+}
+
+func (this *MaxQueue) Push_back(value int) {
+	this.dataQ.PushBack(value)
+
+	if this.maxQ.Len() == 0 || value >= this.Max_value() {
+		this.maxQ.PushFront(value)
+	}
+}
+
+func (this *MaxQueue) Pop_front() int {
+	if this.dataQ.Len() <= 0 {
+		return -1
+	}
+
+	f := this.dataQ.Front()
+	this.dataQ.Remove(f)
+
+	v := f.Value.(int)
+
+	if v == this.Max_value() {
+		this.maxQ.Remove(this.maxQ.Front())
+	}
+
+	return v
+}
+
+/**
+ * Your MaxQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Max_value();
+ * obj.Push_back(value);
+ * param_3 := obj.Pop_front();
  */
