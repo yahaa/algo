0c5e2e94b4a489818adb1a71a5d1386a9adb10de
yahaa <1477765176@qq.com>
2023-09-02 23:22:01 +0800

    leetcode 210

--- algo-go/topo/topo.go
+++ algo-go/topo/topo.go
@@ -38,3 +38,42 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 
 	return len(result) == numCourses
 }
+
+// findOrder leetcode 210 课程表 2
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	g := make(map[int][]int)
+	in := make(map[int]int)
+	queue := list.New()
+	result := make([]int, 0)
+
+	for _, item := range prerequisites {
+		g[item[1]] = append(g[item[1]], item[0])
+		in[item[0]]++
+	}
+
+	for i := 0; i < numCourses; i++ {
+		if in[i] == 0 {
+			queue.PushFront(i)
+		}
+	}
+
+	for queue.Len() > 0 {
+		u := queue.Front().Value.(int)
+		queue.Remove(queue.Front())
+		result = append(result, u)
+
+		for _, v := range g[u] {
+			in[v]--
+
+			if in[v] == 0 {
+				queue.PushFront(v)
+			}
+		}
+	}
+
+	if len(result) == numCourses {
+		return result
+	}
+
+	return []int{}
+}
