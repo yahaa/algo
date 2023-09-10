1ae0196367ffed50dfcf9d517f069baec57b5658
yahaa <1477765176@qq.com>
2023-09-10 20:59:49 +0800

    128. 最长连续序列

--- algo-go/hash/hash.go
+++ algo-go/hash/hash.go
@@ -1,5 +1,44 @@
 package hash
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// longestConsecutive 128. 最长连续序列
+func longestConsecutive(nums []int) int {
+	set := make(map[int]struct{})
+
+	for _, n := range nums {
+		set[n] = struct{}{}
+	}
+
+	ans := 0
+
+	for n := range set {
+		if _, ok := set[n-1]; !ok {
+			cur := n
+			curLen := 1
+
+			for {
+				_, ok := set[cur+1]
+				if !ok {
+					break
+				}
+
+				cur++
+				curLen++
+			}
+
+			ans = max(ans, curLen)
+		}
+	}
+	return ans
+}
+
 // romanToInt leetcode 13. 罗马数字转整数
 func romanToInt(s string) int {
 	m := map[string]int{
