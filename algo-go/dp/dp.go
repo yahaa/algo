b86f3115d1ed97a22480b39cd319cdcb2f40353d
yahaa <1477765176@qq.com>
2023-09-17 21:59:54 +0800

    198. 打家劫舍

--- algo-go/dp/dp.go
+++ algo-go/dp/dp.go
@@ -228,3 +228,27 @@ func findLongestFromIndex(matrix [][]int, i, j int) int {
 func longestIncreasingPath(matrix [][]int) int {
 	return 0
 }
+
+// rob 198. 打家劫舍
+func rob(nums []int) int {
+	// dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, len(nums))
+
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+
+	return dp[len(nums)-1]
+}
