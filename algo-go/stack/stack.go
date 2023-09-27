b60c35e7bcba705dbb66ed425879fe179c60f1c6
yahaa <1477765176@qq.com>
2023-09-27 23:32:08 +0800

    1598. 文件夹操作日志搜集器 stack 简单应用

--- algo-go/stack/stack.go
+++ algo-go/stack/stack.go
@@ -1,5 +1,30 @@
 package stack
 
+import "container/list"
+
 func longestValidParentheses(s string) int {
 	return 0
+}
+
+// minOperations 1598. 文件夹操作日志搜集器
+func minOperations(logs []string) int {
+	stack := list.New()
+
+	for _, item := range logs {
+		if item == "./" {
+			continue
+		}
+
+		if item == "../" {
+			if stack.Len() > 0 {
+				stack.Remove(stack.Front())
+			}
+
+			continue
+		}
+
+		stack.PushFront(item)
+	}
+
+	return stack.Len()
 }
