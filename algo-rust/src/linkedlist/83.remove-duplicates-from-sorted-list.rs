99af252cb95df8ed1c6f81aaf4509632119815ad
yahaa <1477765176@qq.com>
2023-12-19 23:10:50 +0800

    add leetcode 203 rust impl

--- algo-rust/src/linkedlist/83.remove-duplicates-from-sorted-list.rs
+++ algo-rust/src/linkedlist/83.remove-duplicates-from-sorted-list.rs
@@ -13,21 +13,21 @@
  *
  * Given a sorted linked list, delete all duplicates such that each element
  * appear only once.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: 1->1->2
  * Output: 1->2
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: 1->1->2->3->3
  * Output: 1->2->3
- * 
- * 
+ *
+ *
  */
 // Definition for singly-linked list.
 // #[derive(PartialEq, Eq, Clone, Debug)]
@@ -35,7 +35,7 @@
 //   pub val: i32,
 //   pub next: Option<Box<ListNode>>
 // }
-// 
+//
 // impl ListNode {
 //   #[inline]
 //   fn new(val: i32) -> Self {
@@ -46,6 +46,23 @@
 //   }
 // }
 impl Solution {
+    pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode::new(0));
+        dummy.next = head;
+
+        let mut pre = dummy.as_mut();
+
+        while let Some(cur) = pre.next.as_mut() {
+            if cur.val == val {
+                pre.next = cur.next.take();
+            } else {
+                pre = pre.next.as_mut().unwrap();
+            }
+        }
+
+        dummy.next
+    }
+
     pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         if head.is_none() {
             return None;
