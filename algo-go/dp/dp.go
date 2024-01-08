271f19ec2175643f0e61e779e2fd876f5efe85a3
yahaa <1477765176@qq.com>
2024-01-08 23:19:44 +0800

    leetcode 5 最长回文子串 dp 实现

--- algo-go/dp/dp.go
+++ algo-go/dp/dp.go
@@ -289,3 +289,48 @@ func minCostClimbingStairs(cost []int) int {
 
 	return dp[len(cost)]
 }
+
+// longestPalindrome leetcode 5 最长回文子串 dp 实现
+func longestPalindrome(s string) string {
+	// dp[i][j] 表示 s[i...j] 是否为回文串
+	// dp[i][i] 表示所有长度为 1 的子串
+	if len(s) < 2 {
+		return s
+	}
+
+	dp := make([][]bool, len(s))
+
+	maxLen, start := 1, 0
+
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]bool, len(s))
+		dp[i][i] = true
+	}
+
+	// 遍历所有长度
+	for l := 2; l <= len(s); l++ {
+		for i := 0; i < len(s); i++ {
+			j := l + i - 1
+
+			if j >= len(s) {
+				break
+			}
+
+			if s[i] != s[j] {
+				dp[i][j] = false
+			} else {
+				if j-i < 3 {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = dp[i+1][j-1]
+				}
+			}
+
+			if dp[i][j] && j-i+1 > maxLen {
+				maxLen = j - i + 1
+				start = i
+			}
+		}
+	}
+	return s[start : start+maxLen]
+}
